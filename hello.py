c66481d168018be00c43b756e8a094458eb85d72
John Krowas <jkrowas@gmail.com>
2017-02-18 17:16:41 -0800

    First commit 2 3

--- hello.py
+++ hello.py
@@ -3,9 +3,20 @@ import sys
 from bokeh.io import output_notebook, show
 from bokeh.plotting import figure
 
+from bokeh.models.widgets import Panel, Tabs
+from bokeh.io import output_file, show
+from bokeh.plotting import figure
 
-print("Hello, world!")
-p = figure(plot_width=400, plot_height=400)
-p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=15, line_color="navy", fill_color="orange", fill_alpha=0.5)
+output_file("slider.html")
 
-show(p)
+p1 = figure(plot_width=600, plot_height=600)
+p1.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=20, color="navy", alpha=0.5)
+tab1 = Panel(child=p1, title="circle")
+
+p2 = figure(plot_width=600, plot_height=600)
+p2.line([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], line_width=3, color="navy", alpha=0.5)
+tab2 = Panel(child=p2, title="line")
+
+tabs = Tabs(tabs=[ tab1, tab2 ])
+
+show(tabs)
