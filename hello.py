8f5a1941dbd02c4e77a811033d18b1797eb7a243
John Krowas <jkrowas@gmail.com>
2017-02-18 17:04:43 -0800

    First commit 2 3

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
 import pandas
 import sys
+from bokeh.io import output_notebook, show
+from bokeh.plotting import figure
 
 
 print("Hello, world!")
+p = figure(plot_width=400, plot_height=400)
+p.circle([1, 2, 3, 4, 5], [6, 7, 2, 4, 5], size=15, line_color="navy", fill_color="orange", fill_alpha=0.5)
+
+show(p)
